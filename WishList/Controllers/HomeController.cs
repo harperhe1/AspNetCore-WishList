8fe1f16e8ade0eb89a2565411e8955e2edbf75d7
harperhe1 <harperhe@hotmail.com>
2022-07-21 18:40:39 -0400

    Fixed HomeController

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -4,5 +4,14 @@ namespace WishList.Controllers
 {
     public class HomeController : Controller
     {
+        public IActionResult Index()
+        {
+            return View("/Index");
+        }
+
+        public IActionResult Error()
+        {
+            return View("/Error");
+        }
     }
 }
